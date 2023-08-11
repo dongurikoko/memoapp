d05f5aa4f5dc907833bd92665d3314cdd85cad80
大本詩織 <oomotoshiori@oomotoshiorinoMacBook-Pro.local>
2023-08-12 00:05:45 +0900

    initial

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  get "posts/:id/destroy" => "posts#destroy"
+  
+
+  get "/" => "home#top"
+  get "about" => "home#about"
+
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
